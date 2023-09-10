8e4a47407b2d92ad36106ac55b2ec7f2fcdfb290
Nicolas Wulff-Kähler <nico@mmx.dk>
2023-09-10 23:57:46 +0200

    Add project files.

---
+++ Biograf Billetter/Order.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization.Metadata;
+using System.Threading.Tasks;
+
+namespace Biograf_Billetter
+{
+    internal class Order
+    {
+        public int quantity { get; set; }
+        public int funds { get; set; }
+        readonly int price = 120;
+
+        int orderCount = 0;
+
+        // File naming scheme:
+        // order_{index}.txt
+
+        // Some checking to see if we have any previous orders saved
+        public void CheckOrders()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "orders");
+            foreach (string file in Directory.EnumerateFiles(path, "*.txt"))
+            {
+                if (File.Exists(file))
+                {
+                    string fileName = Path.GetFileName(file);
+                    string orderNum = fileName.Split("_")[1].Split(".")[0];
+                    int.TryParse(orderNum, out int orderNumParsed);
+                    if (orderNumParsed > orderCount) { orderCount = orderNumParsed; }
+                }
+            }
+            Console.WriteLine(orderCount);
+        }
+
+        // Init
+        public bool init(int f, int q)
+        {
+            if (q <= 0)
+            {
+                Console.WriteLine("You have to buy a minimum of 1 ticket.");
+                Console.ReadKey();
+                return false;
+            }
+
+            if (f <= 0)
+            {
+                Console.WriteLine("Your funds has to be greater than 0.");
+                Console.ReadKey();
+                return false;
+            }
+
+            if (f < q * price)
+            {
+                Console.WriteLine("Insufficient funds, please insert more funds before incresing ticket quantity.");
+                Console.ReadKey();
+                return false;
+            }
+
+            quantity = q;
+            funds = f;
+
+            return true;
+
+        }
+
+        // Change Quantity
+        public bool ChangeQuantity(int q)
+        {
+            if (q <= 0)
+            {
+                Console.WriteLine("You have to buy a minimum of 1 ticket.");
+                Console.ReadKey();
+                return false;
+            }
+
+            if (funds < q * price)
+            {
+                Console.WriteLine("Insufficient funds, please insert more funds before incresing ticket quantity.");
+                Console.ReadKey();
+                return false;
+            }
+
+            quantity = q;
+            return true;
+        }
+
+        public bool ChangeFunds(int f)
+        {
+            if (f <= 0)
+            {
+                Console.WriteLine("Your funds has to be greater than 0.");
+                Console.ReadKey();
+                return false;
+            }
+
+            if (f < quantity * price)
+            {
+                Console.WriteLine("Insufficient funds, please lower ticket amount before changing lowering funds");
+                Console.ReadKey();
+                return false;
+            }
+
+            funds = f;
+
+            return true;
+        }
+
+        public void SaveOrder()
+        {
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "orders", $"order_{orderCount+1}.txt");
+
+            string[] order =
+            {
+                (quantity*120).ToString(),
+                (funds - quantity*120).ToString(),
+                quantity.ToString(),
+            };
+
+            File.WriteAllLines(path, order);
+        }
+    }
+}
---
+++ Biograf Billetter/Program.cs
@@ -0,0 +1,99 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Biograf_Billetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.CheckOrders();
+
+            bool main = true;
+
+            // Main loop
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter your funds.");
+                if(!int.TryParse(Console.ReadLine(), out int funds))
+                {
+                    Console.WriteLine("Funds invalid, make sure the number is greater than 0");
+                    Console.ReadKey();
+                    continue;
+                }
+                Console.WriteLine("Please enter ticket quantity");
+                if (!int.TryParse(Console.ReadLine(), out int quantity))
+                {
+                    Console.WriteLine("Quantity invalid, make sure the number is greater than 0");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                if(order.init(funds, quantity))
+                {
+                    main = false;
+                }
+
+            } while (main);
+
+            // Giving the opportunity to change their funds and ticket quantity
+            bool changes = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("What do you want to do next?\n" +
+                    "1. Change funds\n" +
+                    "2. Change ticket quantity\n" +
+                    "3. Confirm order");
+                if(!int.TryParse (Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Please enter a number between 1 - 3");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter new funds");
+                        if(!int.TryParse(Console.ReadLine(), out int funds))
+                        {
+                            Console.WriteLine("Funds invalid, make sure the number is greater than 0");
+                            Console.ReadKey();
+                            continue;
+                        }
+                        if (!order.ChangeFunds(funds)) continue;
+                        Console.WriteLine("Changed funds");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Please enter new ticket quantity");
+                        if (!int.TryParse(Console.ReadLine(), out int quantity))
+                        {
+                            Console.WriteLine("Ticket quantity invalid, make sure the number is greater than 0");
+                            Console.ReadKey();
+                            continue;
+                        }
+                        if (!order.ChangeQuantity(quantity)) continue;
+                        Console.WriteLine("Changed quantity");
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Price: {order.quantity*120}\n" +
+                            $"Remaining money: {order.funds - order.quantity*120}\n" +
+                            $"Ticket Quantity: {order.quantity}");
+                        order.SaveOrder();
+                        changes = false;
+                        break;
+                }
+
+            } while (changes);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
